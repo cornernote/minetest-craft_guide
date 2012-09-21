1d6261194c1e3480f479c5f45fd5f31a0b370951
Brett O'Donnell <cornernote@gmail.com>
2012-09-21 16:21:12 +0930

    only show alternate if there is more recipies

--- api_craft_guide.lua
+++ api_craft_guide.lua
@@ -45,32 +45,53 @@ craft_guide.register_craft = function(options)
 end
 
 
--- set_craft_guide_formspec
-craft_guide.set_craft_guide_formspec = function(meta, start_i, pagenum, recipenum)
-	pagenum = math.floor(pagenum)
-	local pagemax = math.floor((craft_guide.craft_guide_size-1) / (5*14) + 1)
-	meta:set_string("formspec",
-		"size[14,10;]"..
-		"label[0,0;--== Learn to Craft ==--]"..
-		"label[0,0.4;Drag an item to the Output box]"..
-		"label[9,0.2;page "..tostring(pagenum).." of "..tostring(pagemax).."]"..
-		"button[11,0;1.5,1;craft_guide_prev;<<]"..
-		"button[12.5,0;1.5,1;craft_guide_next;>>]"..
-		"list[detached:craft_guide;main;0,1;14,5;"..tostring(start_i).."]"..
-		"label[0,6.5;Output]"..
-		"list[current_name;output;0,7;1,1;]"..
-		"label[0.3,8.6;recipe "..tostring(recipenum).."]"..
-		"button[0,9;2,1;alternate;Alternate]"..
-		"label[2,6.5;Inventory Craft]"..
-		"list[current_name;build;2,7;3,3;]"..
-		"label[6,6.5;Cook]"..
-		"list[current_name;cook;6,7;1,1;]"..
-		"label[6,8.5;Fuel]"..
-		"list[current_name;fuel;6,9;1,1;]"..
-		"label[8,6.5;Bookmarks]"..
-		"list[current_name;bookmark;8,7;6,3;]"..
-		"label[12,6.5;Bin ->]"..
-		"list[current_name;bin;13,6;1,1;]")
+-- get_craft_guide_formspec
+craft_guide.get_craft_guide_formspec = function(meta, page, alternate)
+	if page == nil then 
+		page = craft_guide.get_current_page(meta) 
+	end
+	if alternate == nil then 
+		alternate = craft_guide.get_current_alternate(meta) 
+	end
+	local start = (page-1) * (5*14) + 1
+	local pages = math.floor((craft_guide.craft_guide_size-1) / (5*14) + 1)
+	local alternates = 0
+	local stack = meta:get_inventory():get_stack("output",1)
+	local crafts = craft_guide.crafts[stack:get_name()]
+	if crafts ~= nil then
+		alternates = #crafts
+	end
+	local formspec = "size[14,10;]"
+		.."label[0,0;--== Learn to Craft ==--]"
+		.."label[0,0.4;Drag an item to the Output box]"
+		.."label[9,0.2;page "..tostring(page).." of "..tostring(pages).."]"
+		.."button[11,0;1.5,1;craft_guide_prev;<<]"
+		.."button[12.5,0;1.5,1;craft_guide_next;>>]"
+		.."list[detached:craft_guide;main;0,1;14,5;"..tostring(start).."]"
+		.."label[0,6.5;Output]"
+		.."list[current_name;output;0,7;1,1;]"
+		.."label[2,6.5;Inventory Craft]"
+		.."list[current_name;build;2,7;3,3;]"
+		.."label[6,6.5;Cook]"
+		.."list[current_name;cook;6,7;1,1;]"
+		.."label[6,8.5;Fuel]"
+		.."list[current_name;fuel;6,9;1,1;]"
+		.."label[8,6.5;Bookmarks]"
+		.."list[current_name;bookmark;8,7;6,3;]"
+		.."label[12,6.5;Bin ->]"
+		.."list[current_name;bin;13,6;1,1;]"
+	if alternates > 1 then
+		formspec = formspec
+			.."label[0,8.6;recipe "..tostring(alternate).." of "..tostring(alternates).."]"
+			.."button[0,9;2,1;alternate;Alternate]"
+	end
+	return formspec
+end
+
+
+-- on_construct
+craft_guide.on_construct = function(pos)
+	local meta = minetest.env:get_meta(pos)
 	local inv = meta:get_inventory()
 	inv:set_size("output", 1)
 	inv:set_size("build", 3*3)
@@ -78,58 +99,68 @@ craft_guide.set_craft_guide_formspec = function(meta, start_i, pagenum, recipenu
 	inv:set_size("fuel", 1)
 	inv:set_size("bookmark", 6*3)
 	inv:set_size("bin", 1)
-end
-
-
--- on_construct
-craft_guide.on_construct = function(pos)
-	craft_guide.set_craft_guide_formspec(minetest.env:get_meta(pos), 0, 1, 1)
+	meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta))
 end
 
 
 -- on_receive_fields
 craft_guide.on_receive_fields = function(pos, formname, fields, player)
 	local meta = minetest.env:get_meta(pos);
-	local formspec = meta:get_string("formspec")
-	local start_i = string.match(formspec, "list%[detached:craft_guide;main;[%d.]+,[%d.]+;[%d.]+,[%d.]+;(%d+)%]")
-	local alternate_i = string.match(formspec, "label%[[%d.]+,[%d.]+;recipe (%d+)%]")
 
-	-- get an alternate recipe
 	local stack = meta:get_inventory():get_stack("output",1)
 	local crafts = craft_guide.crafts[stack:get_name()]
+	local alternate = craft_guide.get_current_alternate(meta)
+	local alternates = 0
 	if crafts ~= nil then
-		alternate_i = tonumber(alternate_i) or 1
-		if fields.alternate then
-			alternate_i = alternate_i+1
-		end
-		if alternate_i > #crafts then
-			alternate_i = 1
-		end
-		craft_guide.update_recipe(meta, player, stack, alternate_i)
+		alternates = #crafts
 	end
 
-	-- Figure out current page from formspec
-	start_i = tonumber(start_i) or 0
+	local page = craft_guide.get_current_page(meta)
+	local pages = math.floor((craft_guide.craft_guide_size-1) / (5*14) + 1)
 
+	-- get an alternate recipe
+	if fields.alternate then
+		alternate = alternate+1
+		craft_guide.update_recipe(meta, player, stack, alternate)
+	end
+	if alternate > alternates then
+		alternate = 1
+	end
+
+	-- change page
 	if fields.craft_guide_prev then
-		start_i = start_i - 5*14
+		page = page - 1
 	end
 	if fields.craft_guide_next then
-		start_i = start_i + 5*14
+		page = page + 1
+	end
+	if page < 1 then
+		page = 1
+	end
+	if page > pages then
+		page = pages
 	end
 
-	if start_i < 0 then
-		start_i = start_i + 5*14
-	end
-	if start_i >= craft_guide.craft_guide_size then
-		start_i = start_i - 5*14
-	end
-		
-	if start_i < 0 or start_i >= craft_guide.craft_guide_size then
-		start_i = 0
-	end
+	-- update the formspec
+	meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta, page, alternate))
+end
 
-	craft_guide.set_craft_guide_formspec(meta, start_i, start_i / (5*14) + 1, alternate_i)
+
+-- get_current_page
+craft_guide.get_current_page = function(meta)
+	local formspec = meta:get_string("formspec")
+	local page = string.match(formspec, "label%[[%d.]+,[%d.]+;page (%d+) of [%d.]+%]")
+	page = tonumber(page) or 1
+	return page
+end
+
+
+-- get_current_alternate
+craft_guide.get_current_alternate = function(meta)
+	local formspec = meta:get_string("formspec")
+	local alternate = string.match(formspec, "label%[[%d.]+,[%d.]+;recipe (%d+) of [%d.]+%]")
+	alternate = tonumber(alternate) or 1
+	return alternate
 end
 
 
@@ -151,6 +182,7 @@ craft_guide.update_recipe = function(meta, player, stack, alternate)
 	
 	if crafts == nil then
 		minetest.chat_send_player(player:get_player_name(), "no recipe available for "..stack:get_name())
+		meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta))
 		return
 	end
 	if alternate < 1 or alternate > #crafts then
@@ -168,11 +200,13 @@ craft_guide.update_recipe = function(meta, player, stack, alternate)
 	-- cook
 	if craft.type == "cooking" then
 		inv:set_stack("cook", 1, craft.recipe)
+		meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta))
 		return
 	end
 	-- fuel
 	if craft.type == "fuel" then
 		inv:set_stack("fuel", 1, craft.recipe)
+		meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta))
 		return
 	end
 	-- build (shaped or shapeless)
@@ -251,6 +285,7 @@ craft_guide.update_recipe = function(meta, player, stack, alternate)
 			inv:set_stack("build", 9, craft.recipe[9])
 		end
 	end
+	meta:set_string("formspec",craft_guide.get_craft_guide_formspec(meta))
 end
 
 
@@ -305,11 +340,15 @@ craft_guide.allow_metadata_inventory_move = function(pos, from_list, from_index,
 	if from_list == "bookmarks" and to_list == "bookmarks"  then
 		return count
 	end
-	if to_list == "bin" and (from_list == "output" or from_list == "bookmark") then
+	if to_list == "bin" and from_list == "output" then
+		inv:set_stack(from_list,from_index,nil)
+		craft_guide.update_recipe(meta, player, inv:get_stack(from_list, from_index))
+	end
+	if to_list == "bin" and from_list == "bookmark" then
 		inv:set_stack(from_list,from_index,nil)
 	end
-	if to_list == "output" or to_list == "bookmark" then
-		inv:set_stack(to_list, to_index, inv:get_stack(from_list, from_index))
+	if to_list == "bookmark" then
+		inv:set_stack(to_list, to_index, inv:get_stack(from_list, from_index):get_name())
 	end
 	if to_list == "output" then
 		craft_guide.update_recipe(meta, player, inv:get_stack(from_list, from_index))
